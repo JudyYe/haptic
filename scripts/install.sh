39e3f0703511447c5809d789d8f68c4052e44c48
Sriram Krishna <sriramsk1999@gmail.com>
2025-06-16 00:17:20 -0400

    add ViTPose submodule

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,4 +8,6 @@ pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https
 pip install pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py310_cu121_pyt211/download.html
 
 pip install -r requirements.txt
+
+git submodule update --init --recursive
 pip install -v -e third-party/ViTPose
